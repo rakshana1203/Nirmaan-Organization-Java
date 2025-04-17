295d7700bf69e2adc3e063f51703d8c36e2df190
rakshana1203 <113830671+rakshana1203@users.noreply.github.com>
2025-04-17 13:19:34 +0530

    Day-9

---
+++ Student.java
@@ -0,0 +1,18 @@
+package Day10;
+
+public class Student {
+	String name;
+	int regNo;
+
+	public static void main(String[] args) {
+		
+		Student stu1=new Student();
+		stu1.name="Rexa";
+		stu1.regNo=1235;
+		
+		
+		Student stu2=new Student();
+		stu2.name="Rexa";
+		stu2.regNo=1235;
+		System.out.println(stu2.name);
+}}
