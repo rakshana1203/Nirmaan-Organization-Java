6bd8b1a9efd2ca751e59e30390e800996841d5c0
rakshana1203 <113830671+rakshana1203@users.noreply.github.com>
2025-05-03 11:11:27 +0530

    Day-2

---
+++ variable.java
@@ -0,0 +1,30 @@
+package Day2;
+
+public class variable {
+
+	public static void main(String[] args) {
+		
+		byte num =127;
+		Short a = 30767;
+		int b = 2000000000;
+		long c = 98765432125l;
+		String name = "raks";
+		float flo = 12.46f;
+		double d1 =23.43546D;
+		char c1 ='a';
+		boolean isTrue = true;
+		 
+		System.out.println("byte num:" +num  );
+		System.out.println("Short a :" +a  );
+		System.out.println("int b:" +b  );
+		System.out.println("long c :" +c  );
+		System.out.println("String name :" +name );
+		System.out.println("float flo :" +flo   );
+		System.out.println("double d1 :" +d1  );
+		System.out.println("char c1 :" +c1  );
+		System.out.println("boolean isTrue :" +isTrue  );
+		
+		
+		
+	}
+}
