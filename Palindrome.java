e5af86378a517eb283bb5005e8e60a5f30143355
rakshana1203 <113830671+rakshana1203@users.noreply.github.com>
2025-05-09 11:48:36 +0530

    Day-5

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+package Day8;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		
+		String name=" Madam";
+		String rss=name.trim();
+		String rs="";
+		
+		for (int i=rss.length()-1; i>=0; i--) {
+			rs += rss.charAt(i);}
+		{
+			System.out.println(rs);
+		}	
+		
+		}
+		
+	}
+	
+	
+	
+
